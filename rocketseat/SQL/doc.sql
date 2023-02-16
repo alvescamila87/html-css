6bd45102c710c74f07b707d7e9248343aef071b1
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-02-16 19:56:48 -0300

    update aula de banco de dados

--- rocketseat/SQL/doc.sql
+++ rocketseat/SQL/doc.sql
@@ -1,3 +1,5 @@
+/* SQLs alunos [database.sql] */ 
+
 SELECT * FROM aluno WHERE matricula = 1
 
 SELECT * FROM aluno WHERE nome like 'g'
@@ -33,3 +35,28 @@ SELECT * FROM aluno WHERE matricula = 4 / 2
 SELECT * FROM aluno WHERE matricula = 3 % 2
 
 SELECT * FROM aluno WHERE nome like 'j%' AND matricula < 2
+
+SELECT * FROM aluno WHERE matricula = 1 OR nome like 'j%'
+
+INSERT INTO aluno (nome, cpf, responsavel) VALUES ('Camila Ferrari', 16158493178, 'Isabel Orthman')
+
+UPDATE aluno SET cpf=81236985214, responsavel='Paulo João' WHERE matricula = 4
+
+DELETE FROM aluno WHERE matricula = 4
+
+
+/* SQLs Funcionários [unindo_tabelas.sql] */ 
+
+SELECT * FROM funcionarios WHERE id_funcionario = 4
+
+SELECT * FROM funcionarios WHERE id_funcionario BETWEEN 4 AND 7
+
+SELECT * FROM funcionarios WHERE id_funcionario NOT BETWEEN 4 and 7
+
+SELECT * FROM funcionarios WHERE id_departamento IN (1, 3, 5)
+
+SELECT * FROM funcionarios WHERE id_departamento NOT IN (1, 3, 5)
+
+SELECT * FROM funcionarios WHERE id_departamento IS NULL
+
+SELECT * FROM funcionarios WHERE id_departamento IS NOT NULL
