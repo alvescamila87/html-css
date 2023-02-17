2e4507b37956222726e3f4a4011f7b12ecc53ac9
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-02-17 16:17:34 -0300

    update aula programação funcional

---
+++ rocketseat/JAVASCRIPT/aula6_heranca.js
@@ -0,0 +1,15 @@
+// herança
+
+class Veiculo {
+    rodas = 4;
+
+    mover(direcao) {}
+    virar(direcao) {}
+}
+
+class Moto extends Veiculo {
+    constructor() {
+        super() // puxar atributos e métodos do pai
+        this.rodas = 2
+    }
+}
