1ecab8de31e2cd12e70e89ec2ff8277def61f49d
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-04-07 17:49:40 -0300

    update aulas de sql avançado

---
+++ rocketseat/SQL/notes.sql
@@ -0,0 +1,15 @@
+/* Aula de SQL Avançado - Fundamentos
+
+    PrimaryKey: chave de identificação das informação 'id_user', por exemplo, de uma tabela "A". nunca se repete.
+    ForeignKey: utilização da chave de identificação 'id_user' da tabela 'A' para relacionar com a tabela 'B'. Pode se repetir.
+    Uma tabela pode ter mais de uma foreing key, mas não pode ter mais de uma primary key.
+
+*/
+
+/* Aula de SQL Avançado - Comandos 
+
+    -JOIN: unir tabelas
+
+*/
+
+SELECT * FROM 
